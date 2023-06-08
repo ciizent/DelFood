0388900791f0d7a4467d57956789bf9a56173bae
Madiyar <135884531+ciizent@users.noreply.github.com>
2023-06-09 01:26:15 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const buttonAuth = document.querySelector('.button-auth')
+const modalAuth = document.querySelector('.modal-auth')
+
+console.log(document)
+
+// function sayMeow() {
+//     console.log('Meow');
+// }
+// //Анонимная функция
+// const SayMeow = function() {
+//     console.log('Meow');
+// }
+
+//Стрелочная функция
+// const SayMeow = () => {
+//     console.log('Meow');
+// }
+// sayMeow()
+
+// const obj = {
+//     name: 'Alex',
+//     age: 21,
+//     sayHello: function() {
+//         console.log('Здарова,Алекс');
+//     }
+
+// }
+
+// console.log(obj.sayHello());
+
+const arr = [1, 2, 3, 4, 5]
+console.log(arr[2]);
+
+const counter = (a, b) => {
+    return a + b
+}
